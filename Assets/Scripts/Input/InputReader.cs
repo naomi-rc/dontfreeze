851f710e9e1b7f7eaf5955aa7f5cfc3aa85b14a0
Jonathan Bouchard <dev.drakota@gmail.com>
2022-05-25 18:09:18 -0400

    Initialize the project with the "new" input system

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+public class InputReader : ScriptableObject, GameInput.IGameplayActions, GameInput.IUIActions
+{
+    public event UnityAction<Vector2> MoveEvent = delegate { };
+
+    private GameInput gameInput;
+
+    private void OnEnable()
+    {
+        if (gameInput == null)
+        {
+            gameInput = new GameInput();
+
+            gameInput.Gameplay.SetCallbacks(this);
+            gameInput.UI.SetCallbacks(this);
+        }
+    }
+
+    public void EnableGameplayInput()
+    {
+        gameInput.Gameplay.Enable();
+        gameInput.UI.Disable();
+    }
+
+    public void EnableUiInput()
+    {
+        gameInput.Gameplay.Disable();
+        gameInput.UI.Enable();
+    }
+
+    #region Gameplay actions
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent.Invoke(context.ReadValue<Vector2>());
+    }
+    #endregion
+
+    #region UI actions
+    public void OnNavigate(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnSubmit(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnScrollWheel(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnMiddleClick(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnRightClick(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnTrackedDevicePosition(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnTrackedDeviceOrientation(InputAction.CallbackContext context)
+    {
+    }
+    #endregion
+}
