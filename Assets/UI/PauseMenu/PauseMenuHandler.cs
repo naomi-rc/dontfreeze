78657ba01877435d69a4a97bd0d8e5423c07503c
Jonathan Bouchard <dev.drakota@gmail.com>
2022-05-28 12:04:14 -0400

    Implemented the pause menu

--- Assets/UI/PauseMenu/PauseMenuHandler.cs
+++ Assets/UI/PauseMenu/PauseMenuHandler.cs
@@ -10,7 +10,6 @@ public class PauseMenuHandler : MonoBehaviour
     public UnityAction SettingsButtonAction = delegate { };
 
     private Button resumeButton;
-    private Button restartButton;
     private Button settingsButton;
     private Button quitButton;
 
@@ -19,12 +18,10 @@ public class PauseMenuHandler : MonoBehaviour
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
 
         resumeButton = rootVisualElement.Q<Button>("ResumeButton");
-        restartButton = rootVisualElement.Q<Button>("RestartButton");
         settingsButton = rootVisualElement.Q<Button>("SettingsButton");
         quitButton = rootVisualElement.Q<Button>("QuitButton");
 
         resumeButton.clicked += OnResumeButtonClicked;
-        restartButton.clicked += OnRestartButtonClicked;
         settingsButton.clicked += OnSettingsButtonClicked;
         quitButton.clicked += OnQuitButtonClicked;
     }
@@ -32,7 +29,6 @@ public class PauseMenuHandler : MonoBehaviour
     void OnDisable()
     {
         resumeButton.clicked -= OnResumeButtonClicked;
-        restartButton.clicked -= OnRestartButtonClicked;
         settingsButton.clicked -= OnSettingsButtonClicked;
         quitButton.clicked -= OnQuitButtonClicked;
     }
@@ -40,12 +36,6 @@ public class PauseMenuHandler : MonoBehaviour
     void OnResumeButtonClicked()
     {
         ResumeButtonAction.Invoke();
-    }
-
-    void OnRestartButtonClicked()
-    {
-        // TODO: When we setup scene management, restart the game
-        Debug.LogWarning("Unimplemented Restart Button Clicked");
     }
 
     void OnSettingsButtonClicked()
