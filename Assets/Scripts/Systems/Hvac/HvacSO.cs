7a67bde1bd16a629698ec5f2b6bdc8d52944d791
William Lortie <Lortie.w@gmail.com>
2022-05-31 21:56:55 -0400

    Complete refactor of the hvac system

--- Assets/Scripts/Systems/Hvac/HvacSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HvacSO", menuName = "Data/Hvac", order = 0)]
-public class HvacSO : ScriptableObject
-{
-}
--- Assets/Scripts/Systems/Temperature/Receptor.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Receptor : MonoBehaviour
-{
-    public TemperatureSO temperatureSO = default;
-}
