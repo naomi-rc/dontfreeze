a66389f833b53b29db6bbd3e08f54dd860ad93bc
William Lortie <Lortie.w@gmail.com>
2022-06-01 15:32:04 -0400

    Add FloatVariables and FloatReferences

---
+++ Assets/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatVariable", menuName = "Variable/FloatVariable", order = 0)]
+public class FloatVariable : ScriptableObject
+{
+    public float Value;
+}
