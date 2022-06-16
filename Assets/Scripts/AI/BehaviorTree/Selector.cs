909424c9ace6481ee97178033cd90262ab148964
naomi-rc <naomi.cat182128@gmail.com>
2022-06-16 02:22:22 -0400

    added enemy states; implemented tree

--- Assets/Scripts/AI/BehaviorTree/Selector.cs
+++ Assets/Scripts/AI/BehaviorTree/Selector.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Selector : Node
 {
     public Selector() : base("Selector") { }
+    public Selector(string name) : base(name) { }
     public override Status Process()
     {
         foreach (var c in children)
         {
             Status s = c.Process();
             if (s == Status.Running || s == Status.Success)
+            {
+                //Debug.Log($"Node {c.name} is {s}");
                 return s;
+            }
+                
         }
         return Status.Failure;
     }
--- Assets/Scripts/AI/BehaviorTree/Sequence.cs
+++ Assets/Scripts/AI/BehaviorTree/Sequence.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Sequence : Node
 {
     public Sequence() : base("Sequence") { }
+    public Sequence(string name) : base(name) { }
     public override Status Process()
     {
         foreach (var c in children)
@@ -12,7 +13,7 @@ public class Sequence : Node
             Status s = c.Process();
             if (s == Status.Running || s == Status.Failure)
             {
-                Debug.Log($"Node {c.name} is {s}");
+                //Debug.Log($"Node {c.name} is {s}");
                 return s;
             }
                 
