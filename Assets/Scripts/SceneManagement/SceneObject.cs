8fe31fc413a582fede85f1fcab57ce18ef2a616a
William Lortie <lortie.w@gmail.com>
2022-06-19 20:40:41 -0400

    Implement scene management logic

---
+++ Assets/Scripts/SceneManagement/SceneObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+
+[CreateAssetMenu(fileName = "SceneObject", menuName = "Scene/SceneObject", order = 0)]
+public class SceneObject : ScriptableObject
+{
+    [SerializeField]
+    private AssetReference _reference;
+
+    public AssetReference reference
+    {
+        get => _reference;
+    }
+
+    public string GetName()
+    {
+        return _reference.editorAsset.name;
+    }
+
+    // Other scene related data
+}
