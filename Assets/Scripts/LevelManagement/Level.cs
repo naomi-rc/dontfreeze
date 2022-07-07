8e275bb9b3dd31303c1d1905f0b84e6e559fbd99
Dji129 <59750192+Dji129@users.noreply.github.com>
2022-07-07 09:23:27 -0400

    Début du système de sélection

---
+++ Assets/Scripts/LevelManagement/Level.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Script pour définir les paramètres d'un niveau
+}
---
+++ Assets/Scripts/LevelManagement/LevelSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSettings : MonoBehaviour
+{
+   // Script pour premettre les différents choix de paramètres pour les niveaux
+   // On permet le choix de Skybox
+   // Niveau de difficulté
+   // Choix du nombre d'animaux
+   // Choix du nombre de wisp
+
+   // Dans le futur sûrement le choix de la music de background, un personnage différent
+
+    // Méthode qui permet de générer le niveau avec les différents paramètres
+    public void GenerateLevel()
+    {
+
+    }
+}
