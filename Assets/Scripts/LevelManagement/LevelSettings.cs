684babda04a4458473e82a33b783e9e7171555c5
Dji129 <59750192+Dji129@users.noreply.github.com>
2022-07-08 15:26:41 -0400

    Début sélection paramètres visuellement

--- Assets/Scripts/LevelManagement/LevelSettings.cs
+++ Assets/Scripts/LevelManagement/LevelSettings.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class LevelSettings : MonoBehaviour
 {
-   // Script pour premettre les différents choix de paramètres pour les niveaux
-   // On permet le choix de Skybox
-   // Niveau de difficulté
-   // Choix du nombre d'animaux
-   // Choix du nombre de wisp
+    // Script pour premettre les différents choix de paramètres pour les niveaux
+    // On permet le choix de Skybox
+    // Niveau de difficulté
+    // Choix du nombre d'animaux
+    // Choix du nombre de wisp
 
-   // Dans le futur sûrement le choix de la music de background, un personnage différent
+    // Dans le futur sûrement le choix de la music de background, un personnage différent
 
     // Méthode qui permet de générer le niveau avec les différents paramètres
+
+    public Level level;
     public void GenerateLevel()
     {
 
