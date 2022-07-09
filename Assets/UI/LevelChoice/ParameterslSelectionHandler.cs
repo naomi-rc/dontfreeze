49a80fa8b66c388ef4e41c9c40f7ed25fb939711
Dji129 <59750192+Dji129@users.noreply.github.com>
2022-07-08 22:45:29 -0400

    Gestion de base pour la sélection des paramètres

--- Assets/UI/LevelChoice/ParameterslSelectionHandler.cs
+++ Assets/UI/LevelChoice/ParameterslSelectionHandler.cs
@@ -12,12 +12,18 @@ public class ParameterslSelectionHandler : MonoBehaviour
 
     private Button backButton;
     private Button applyButton;
+    private DropdownField skyboxDropDown;
 
-    private RadioButton world1Button;
-    private RadioButton world2Button;
-    private RadioButton world3Button;
-    private RadioButton world4Button;
-    private RadioButton world5Button;
+    private RadioButton easyButton;
+    private RadioButton normalButton;
+    private RadioButton hardButton;
+
+    private SliderInt enemyNumber;
+
+    //private RadioButtonGroup buttonGroup;
+   
+
+    private List<string> skyboxList = new List<string>{ "Daylight", "Nightlight", "DarkMoon", "MoonNight" };
 
     private
     void OnEnable()
@@ -26,13 +32,19 @@ public class ParameterslSelectionHandler : MonoBehaviour
 
         backButton = rootVisualElement.Q<Button>("BackButton"); 
         applyButton = rootVisualElement.Q<Button>("ApplyButton");
-        /*
-        world1Button = rootVisualElement.Q<RadioButton>("World1Button");
-        world2Button = rootVisualElement.Q<RadioButton>("World2Button");
-        world3Button = rootVisualElement.Q<RadioButton>("World3Button");
-        world4Button = rootVisualElement.Q<RadioButton>("World4Button");
-        world5Button = rootVisualElement.Q<RadioButton>("World5Button");
-        */
+        skyboxDropDown = rootVisualElement.Q<DropdownField>("SkyboxList");
+        skyboxDropDown.choices = skyboxList;
+        skyboxDropDown.value = skyboxList[0]; // Default value
+
+        easyButton = rootVisualElement.Q<RadioButton>("EasyButton");
+        normalButton = rootVisualElement.Q<RadioButton>("NormalButton");
+        hardButton = rootVisualElement.Q<RadioButton>("HardButton");
+
+        enemyNumber = rootVisualElement.Q<SliderInt>("EnemyNumber");
+        setMinEnemyValue(15); // Default minimum value
+
+        //buttonGroup = rootVisualElement.Q<RadioButtonGroup>("DifficultyGroup");
+
         backButton.clicked += OnBackButtonClicked; 
         applyButton.clicked += OnApplyButtonClicked;
     }
@@ -51,8 +63,48 @@ public class ParameterslSelectionHandler : MonoBehaviour
 
     void OnApplyButtonClicked()
     {
-        //string world = getWorldSelection();
-        Debug.Log("Bouton apply appuyé");
         ApplyButtonAction.Invoke();
-    }  
+        Debug.Log("Difficulté : " + getDifficultyChoicie());
+        Debug.Log("Skybox choisi : " + getSkybox());
+        Debug.Log("Nombre d'enemy choisi : " + getEnemyNumber());
+    }
+
+    private void setMinEnemyValue(int value)
+    {
+        enemyNumber.lowValue = value;
+    }
+
+    private string getDifficultyChoicie()
+    {
+        string difficulty = "Normal";
+        
+        if (easyButton.value)
+        {
+            difficulty = "Easy";
+            setMinEnemyValue(10);
+        }
+        if (normalButton.value)
+        {
+            difficulty = "Normal";
+            setMinEnemyValue(15);
+        }
+        if (hardButton.value)
+        {
+            difficulty = "Hard";
+            setMinEnemyValue(20);
+        }
+        
+        return difficulty;
+    }
+
+    private string getSkybox()
+    {
+        string skybox = skyboxDropDown.value;
+        return skybox;
+    }
+
+    private int getEnemyNumber()
+    {
+        return enemyNumber.value;
+    }
 }
