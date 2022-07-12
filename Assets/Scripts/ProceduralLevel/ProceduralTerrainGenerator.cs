792b02d4aedb873c1db663b1923b208d750fad81
naomi-rc <naomi.cat182128@gmail.com>
2022-07-11 20:10:39 -0400

    added terrain assets and samples
    # Conflicts:
    #	Assets/Prefabs.meta

--- Assets/Scripts/ProceduralLevel/ProceduralTerrainGenerator.cs
+++ Assets/Scripts/ProceduralLevel/ProceduralTerrainGenerator.cs
@@ -8,74 +8,90 @@ public class ProceduralTerrainGenerator : MonoBehaviour
     float baseDepth = 2f;
 
     Mesh mesh;
-
-    Vector3[] vertices;
+    List<Vector3> verticesList;
     int[] triangles;
 
     public int xSize = 20;
     public int zSize = 20;
+    float pathPositionZ;
+    float xSizeMidpoint;
+    int pathSize = 5;
+    int pathHalfLength = 200;
 
     void Start()
     {
         mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         GetComponent<MeshFilter>().mesh = mesh;
-
+        verticesList = new List<Vector3>();
+        pathPositionZ = (float)zSize / 4 * 3;
+        xSizeMidpoint = xSize / 2;
         //StartCoroutine(CreateShape());
-        CreateShape();
+        //StartCoroutine(CreateSharpShape());
+        CreateSharpShape();
         UpdateMesh();
     }
 
-    void CreateShape()
+    void CreateSharpShape()
     {
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-
-        for (int index = 0, z = 0; z <= zSize; z++)
-        {
-            for (int x = 0; x <= xSize; x++)
-            {
-                float y = CalculateHeight(x,z); 
-                vertices[index] = new Vector3(x, y, z);
-                index++;
-            }
-        }
-
-        int vert = 0;
-        int tris = 0;
-
+        int index = 0;
         triangles = new int[xSize * zSize * 6];
         for (int z = 0; z < zSize; z++)
         {
             for (int x = 0; x < xSize; x++)
             {
-                triangles[tris] = vert;
-                triangles[tris + 1] = vert + xSize + 1;
-                triangles[tris + 2] = vert + 1;
-               // yield return new WaitForSeconds(0f);
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
+                
 
-                vert++;
-                tris += 6;
-                //yield return new WaitForSeconds(0f);
-            }
-            //vert++;
-            vert = xSize * z + z;
+                float height0 = CalculateHeight(x, z);
+                float height1 = CalculateHeight(x, z + 1);
+                float height3 = CalculateHeight(x + 1, z);
+                float height4 = CalculateHeight(x + 1, z + 1);
+
+                verticesList.Add(new Vector3(x, height0, z));
+                verticesList.Add(new Vector3(x, height1, z+1));
+                verticesList.Add(new Vector3(x+1, height3, z));
+
+                triangles[index] = index++;
+                triangles[index] = index++;
+                triangles[index] = index++;
+                /*
+                 verticesList.Add(new Vector3(0, CalculateHeight(0,0), 0));
+                verticesList.Add(new Vector3(0, CalculateHeight(0,1), 1));
+                verticesList.Add(new Vector3(1, CalculateHeight(1,0), 0));
+                 */
+
+
+
+                verticesList.Add(new Vector3(x+1, height3, z));
+                verticesList.Add(new Vector3(x, height1, z+1));
+                verticesList.Add(new Vector3(x+1, height4, z+1));
+                triangles[index] = index++;
+                triangles[index] = index++;
+                triangles[index] = index++;
+                //yield return new WaitForSeconds(0.0001f);
+                /*
+                 verticesList.Add(new Vector3(1, CalculateHeight(0, 0), 0));
+                verticesList.Add(new Vector3(0, CalculateHeight(0, 1), 1));
+                verticesList.Add(new Vector3(1, CalculateHeight(1,1), 1));
+                */
+            }            
         }
-
     }
 
     float CalculateHeight(int x, int z)
     {
-        //float heightMultiplier = 0.04f * Mathf.Pow(x - (float)xSize / 2, 2) + baseDepth;
-        return Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+        //float heightMultiplier = 0.04f * (x - (float)xSize / 2) * (x - (float)xSize / 2) + baseDepth;
+        float heightMultiplier = 3f;
+        if (pathPositionZ - pathSize < z && z < pathPositionZ + pathSize && xSizeMidpoint - pathHalfLength < x && x < xSizeMidpoint + pathHalfLength)
+            heightMultiplier = 0.6f;
+        return Mathf.PerlinNoise(x * .3f, z * .3f) * heightMultiplier;
     }
 
     void UpdateMesh()
     {
         mesh.Clear();
 
-        mesh.vertices = vertices;
+        mesh.vertices = verticesList.ToArray(); //vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
     }
