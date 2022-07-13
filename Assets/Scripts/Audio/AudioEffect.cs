efdcf0ea1a655aaac7ca5afd6ad4a0f1428d804c
Dji129 <59750192+Dji129@users.noreply.github.com>
2022-07-12 22:21:28 -0400

    Commentaire + log enlever

--- Assets/Scripts/Audio/AudioEffect.cs
+++ Assets/Scripts/Audio/AudioEffect.cs
@@ -7,18 +7,15 @@ public class AudioEffect : MonoBehaviour
     private void Step()
     {
         FindObjectOfType<AudioManager>().Play("Moving");
-        //Debug.Log("Step true");
     }
 
     private void Land()
     {
         FindObjectOfType<AudioManager>().Play("Landing");
-        //Debug.Log("Landing true");
     }
 
     private void Jump()
     {
         FindObjectOfType<AudioManager>().Play("Jump");
-        Debug.Log("Jumping true");
     }
 }
