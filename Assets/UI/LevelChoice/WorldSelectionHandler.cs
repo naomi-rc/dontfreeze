532067ffee4ac6f6377f99e4e5d0f7e5e653b9b1
Dji129 <59750192+Dji129@users.noreply.github.com>
2022-07-20 17:27:14 -0400

    Début amélioration carte pour choix niveau

--- Assets/UI/LevelChoice/WorldSelectionHandler.cs
+++ Assets/UI/LevelChoice/WorldSelectionHandler.cs
@@ -9,18 +9,34 @@ public class WorldSelectionHandler : MonoBehaviour
     public UnityAction NextButtonAction = delegate { };
 
     private Button nextButton;
+
+    private string world;
+
+    private RadioButton firstButton;
+    private RadioButton secondButton;
+    private RadioButton thirdButton;
+    private RadioButton fourthButton;
+    private RadioButton fifthButton;
+
     private RadioButtonGroup worldChoices;
 
-    private List<string> worldList = new List<string> { "World 1", "World 2", "World 3", "World 4", "World 5" };
+    //private List<string> worldList = new List<string> { "World 1", "World 2", "World 3", "World 4", "World 5" };
 
     void OnEnable()
     {
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
 
         nextButton = rootVisualElement.Q<Button>("NextButton");
-        worldChoices = rootVisualElement.Q<RadioButtonGroup>("Choices");
-        worldChoices.choices = worldList;
-        worldChoices.value = 0;
+        worldChoices = rootVisualElement.Q<RadioButtonGroup>("WorldChoices");
+
+        firstButton = rootVisualElement.Q<RadioButton>("FirstWorldButton");
+        secondButton = rootVisualElement.Q<RadioButton>("SecondWorldButton");
+        thirdButton = rootVisualElement.Q<RadioButton>("ThirdWorldButton");
+        fourthButton = rootVisualElement.Q<RadioButton>("FourthWorldButton");
+        fifthButton = rootVisualElement.Q<RadioButton>("FifthWorldButton");
+
+        //worldChoices.choices = worldList;
+        //worldChoices.value = 0;
         nextButton.clicked += OnNextButtonClicked;
     }
 
@@ -34,14 +50,31 @@ public class WorldSelectionHandler : MonoBehaviour
         setWorld(worldChoices.value);
         NextButtonAction.Invoke();
     }
+    
     public string getWorldSelectionString()
     {
-        return worldList[worldChoices.value];
+        //return worldList[worldChoices.value];
+        return world;
     }
+
 
     public int getWorldSelection()
     {
-        return worldChoices.value;
+        int i = 0;
+
+        if (firstButton.value)
+        {
+            Debug.Log("Premier monde sélectionner");
+            this.world = "First world";
+        }
+        if (secondButton.value)
+        {
+            Debug.Log("Deuxième monde sélectionner");
+            this.world = "Second world";
+        }
+
+        //return worldChoices.value;
+        return i;
     }
 
     public void setWorld(int world)
